c2b489dbc07916d9e7c3b3af69ef47280af375a9
eiu-NguyenDucDuy <duy.nguyenduc.cit21@eiu.edu.vn>
2024-11-30 09:32:09 +0700

    Update print-name

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         String name = "Duy";
         System.out.println("Welcome to " + name + "'s Java Repository.");
+        System.out.println("Hello " + name + ".");
+        System.out.println("Welcome to my repository.");
     }
 }
